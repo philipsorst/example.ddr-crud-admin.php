37eb00a147abf8a1c0c827089421ec12c4cb68a3
Philip Washington Sorst <philip@sorst.net>
2020-08-02 20:08:04 +0200

    Adding User and BlogPost entity

---
+++ src/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ *
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", nullable=false)
+     */
+    private ?int $id = null;
+}
